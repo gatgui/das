0932c0f3cf783e576f3b7e279afbd61fe9d5d88f
Gaetan Guidet <gatgui@gmail.com>
2018-07-03 17:01:13 +0900

    update global validation state

--- tests/test010/__init__.py
+++ tests/test010/__init__.py
@@ -26,11 +26,31 @@ class TestCase(unittest.TestCase):
 
    # Test functions
 
-   def testValidate(self):
+   def testMake(self):
       with self.assertRaises(das.ValidationError):
          das.make("validation.SomeType", value_pairs={"weight": 1.0})
+
+   def testAssignInvalid(self):
       v = das.make_default("validation.SomeType")
       with self.assertRaises(das.ValidationError):
-        v.value_pairs["weight"] = 1.0
+         v.value_pairs["weight"] = 1.0
+
+   def testAssignValid(self):
+      v = das.make_default("validation.SomeType")
       v.valid_keys.append("weight")
       v.value_pairs["weight"] = 2.0
+
+   def testAssignInvalid2(self):
+      v = das.make_default("validation.SomeType")
+      v.valid_keys.extend(["weight", "index"])
+      v.accepted_values.real = False
+      v.value_pairs["index"] = 10
+      with self.assertRaises(das.ValidationError):
+         v.value_pairs["weight"] = 2.0
+
+   def testSetInvalid(self):
+      v = das.make_default("validation.SomeType")
+      v.valid_keys.extend(["weight", "index"])
+      v.accepted_values.real = False
+      with self.assertRaises(das.ValidationError):
+         v.value_pairs = {"index": 10, "weight": 2.0}
--- tests/test010/testvalidation.py
+++ tests/test010/testvalidation.py
@@ -8,10 +8,18 @@ class SomeTypeValidator(das.Mixin):
    def __init__(self, *args, **kwargs):
       super(SomeTypeValidator, self).__init__(*args, **kwargs)
 
-   def _schema_validation(self):
-      for k in self.value_pairs.keys():
+   def _validate_globally(self):
+      for k, v in self.value_pairs.iteritems():
          if not k in self.valid_keys:
             raise Exception("Invalid key '%s'" % k)
+         if not self.accepted_values.boolean and isinstance(v, bool):
+            raise Exception("Boolean not allowed")
+         if not self.accepted_values.integer and isinstance(v, (int, long)):
+            raise Exception("Integer not allowed")
+         if not self.accepted_values.real and isinstance(v, float):
+            raise Exception("Real not allowed")
+         if not self.accepted_values.string and isinstance(v, basestring):
+            raise Exception("String not allowed")
 
 
 das.register_mixins(SomeTypeValidator)
