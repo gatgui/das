cd6e58cd95d2cab06dad686490a1e531ba7379a1
Gaetan Guidet <gatgui@protonmail.com>
2021-07-24 14:28:39 +0900

    implemented boolean, integer schema type compatibility tests

--- tests/test022/__init__.py
+++ tests/test022/__init__.py
@@ -26,10 +26,75 @@ class TestCase(unittest.TestCase):
 
    # Test functions
    def testBoolean(self):
-      for k0 in das.list_schema_types("compat"):
-         v0 = das.get_schema_type(k0)
-         for k1 in das.list_schema_types("compat"):
-            if k1 == k0:
-               continue
-            v1 = das.get_schema_type(k1)
-            print("%s / %s -> %s" % (k0, k1, v0.is_type_compatible(v1)))
+      k0 = "compat.boolean"
+      v0 = das.get_schema_type(k0)
+      for k1 in das.list_schema_types("compat"):
+         v1 = das.get_schema_type(k1)
+         rv = v0.is_type_compatible(v1)
+         if k1 == k0:
+            self.assertTrue(rv)
+         else:
+            self.assertFalse(rv)
+
+   def testInteger(self):
+      k0 = "compat.integer"
+      v0 = das.get_schema_type(k0)
+      for k1 in das.list_schema_types("compat"):
+         v1 = das.get_schema_type(k1)
+         rv = v0.is_type_compatible(v1)
+         if k1.startswith("compat.integer"):
+            if "Enum" in k1:
+               self.assertFalse(rv)
+            else:
+               self.assertTrue(rv)
+         else:
+            self.assertFalse(rv)
+
+   def testIntegerMin(self):
+      st = das.get_schema_type("compat.integerMin1")
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integer")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMin2")))
+      self.assertTrue(st.is_type_compatible(das.get_schema_type("compat.integerMin3")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMax1")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMax2")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMax3")))
+      self.assertTrue(st.is_type_compatible(das.get_schema_type("compat.integerRange1")))
+      self.assertTrue(st.is_type_compatible(das.get_schema_type("compat.integerRange2")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerRange3")))
+      self.assertTrue(st.is_type_compatible(das.get_schema_type("compat.integerRange4")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerRange5")))
+
+   def testIntegerMax(self):
+      st = das.get_schema_type("compat.integerMax1")
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integer")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMax2")))
+      self.assertTrue(st.is_type_compatible(das.get_schema_type("compat.integerMax3")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMin1")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMin2")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMin3")))
+      self.assertTrue(st.is_type_compatible(das.get_schema_type("compat.integerRange1")))
+      self.assertTrue(st.is_type_compatible(das.get_schema_type("compat.integerRange2")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerRange3")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerRange4")))
+      self.assertTrue(st.is_type_compatible(das.get_schema_type("compat.integerRange5")))
+
+   def testIntegerRange(self):
+      st = das.get_schema_type("compat.integerRange1")
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integer")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMin1")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMin2")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMin3")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMax1")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMax2")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerMax3")))
+      self.assertTrue(st.is_type_compatible(das.get_schema_type("compat.integerRange2")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerRange3")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerRange4")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerRange5")))
+
+   def testIntegerEnum(self):
+      st = das.get_schema_type("compat.integerEnum1")
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integer")))
+      self.assertTrue(st.is_type_compatible(das.get_schema_type("compat.integerEnum1")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerEnum2")))
+      self.assertFalse(st.is_type_compatible(das.get_schema_type("compat.integerEnum3")))
